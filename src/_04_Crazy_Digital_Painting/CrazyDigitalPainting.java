baf429e75baf85e7429ab4c7789f8a86eca6725e
zhaoyd5268 <zhaoyd5628@gmail.com>
2022-12-10 11:27:36 -0800

    Start with Conways_Game_of_Life(Module 3, level 2, project 7 of 8 in module.)

--- src/_04_Crazy_Digital_Painting/CrazyDigitalPainting.java
+++ src/_04_Crazy_Digital_Painting/CrazyDigitalPainting.java
@@ -3,38 +3,38 @@ package _04_Crazy_Digital_Painting;
 import java.awt.Color;
 
 public class CrazyDigitalPainting {
-    // 1. Create two final static integers for the width and height of the display.
+	// 1. Create two final static integers for the width and height of the display.
 	final static int WIDTH = 1000;
 	final static int HEIGHT = 1038;
 
-    // 2. Create a 2D array of Color objects. You will need to import
-    //    java.awt.Color. Initialize the size of the array using the 
-    //    integers created in step 1.
-Color[][] array2d = new Color[WIDTH][HEIGHT];
+	// 2. Create a 2D array of Color objects. You will need to import
+	// java.awt.Color. Initialize the size of the array using the
+	// integers created in step 1.
+	Color[][] array2d = new Color[WIDTH][HEIGHT];
 
+	public CrazyDigitalPainting() {
+		// 3. Open the crazy_digital_painting.png file and look at the image.
 
-    public CrazyDigitalPainting() {
-        // 3. Open the crazy_digital_painting.png file and look at the image.
+		// 4. Iterate through the 2D array and initialize each Color object
+		// to a new color. The sample image was created using the following
+		// pattern:
+		// colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
 
-        // 4. Iterate through the 2D array and initialize each Color object
-        //    to a new color. The sample image was created using the following 
-        //    pattern:
-        //    colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
+		// 5. Come up with your own pattern to make a cool crazy image.
+		for (int i = 0; i < array2d.length; i++) {
+			for (int j = 0; j < array2d[i].length; j++) {
+				array2d[i][j] = new Color(i % 22, (i * j) % 256, j % 212);
+			}
 
-        // 5. Come up with your own pattern to make a cool crazy image.
-    	for(int i = 0; i< array2d.length-1; i++) {
-    		for(int j = 0; j < array2d[i].length; j++) {
-    			array2d[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
-    	        ColorArrayDisplayer.displayColorsAsImage(null);
-    		}
-    	}
+		}
+		ColorArrayDisplayer.displayColorsAsImage(array2d);
 
-        // 6. Use the ColorArrayDisplayer class to call the displayColorsAsImage method 
-        //    to show off your picture.
-    }
+		// 6. Use the ColorArrayDisplayer class to call the displayColorsAsImage method
+		// to show off your picture.
+	}
 
-    public static void main(String[] args) {
-        new CrazyDigitalPainting();
-        
-    }
+	public static void main(String[] args) {
+		new CrazyDigitalPainting();
+
+	}
 }
--- src/_05_Pixel_Art/GridPanel.java
+++ src/_05_Pixel_Art/GridPanel.java
@@ -6,55 +6,67 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-public class GridPanel extends JPanel{
+public class GridPanel extends JPanel {
 
-    private static final long serialVersionUID = 1L;
-    private int windowWidth;
-    private int windowHeight;
-    private int pixelWidth;
-    private int pixelHeight;
-    private int rows;
-    private int cols;
+	private static final long serialVersionUID = 1L;
+	private int windowWidth;
+	private int windowHeight;
+	private int pixelWidth;
+	private int pixelHeight;
+	private int rows;
+	private int cols;
 
-    // 1. Create a 2D array of pixels. Do not initialize it yet.
+	// 1. Create a 2D array of pixels. Do not initialize it yet.
+	public Pixel[][] array2d;
+	private Color color;
 
-    private Color color;
+	public GridPanel(int w, int h, int r, int c) {
+		this.windowWidth = w;
+		this.windowHeight = h;
+		this.rows = r;
+		this.cols = c;
 
-    public GridPanel(int w, int h, int r, int c) {
-        this.windowWidth = w;
-        this.windowHeight = h;
-        this.rows = r;
-        this.cols = c;
+		this.pixelWidth = windowWidth / cols;
+		this.pixelHeight = windowHeight / rows;
 
-        this.pixelWidth = windowWidth / cols;
-        this.pixelHeight = windowHeight / rows;
+		color = Color.BLACK;
 
-        color = Color.BLACK;
+		setPreferredSize(new Dimension(windowWidth, windowHeight));
 
-        setPreferredSize(new Dimension(windowWidth, windowHeight));
+		// 2. Initialize the pixel array using the rows and cols variables.
+			array2d = new Pixel[rows][cols];
+		// 3. Iterate through the array and initialize each element to a new pixel.
+			for(int i = 0; i < array2d[rows-1].length; i++) {
+				for(int j = 0; j < array2d[cols-1].length; j++) {
+					array2d[i][j] = new Pixel(i*pixelWidth, j*pixelHeight);
+				}
+			}
+	}
 
-        // 2. Initialize the pixel array using the rows and cols variables.
+	public void setColor(Color c) {
+		color = c;
+	}
 
+	public void clickPixel(int mouseX, int mouseY) {
+		// 5. Use the mouseX and mouseY variables to change the color
+		// of the pixel that was clicked. *HINT* Use the pixel's dimensions.
+		array2d[mouseX/pixelWidth][mouseY/pixelHeight].color = color; 
+		
+	}
 
-        // 3. Iterate through the array and initialize each element to a new pixel.
+	@Override
+	public void paintComponent(Graphics g) {
+		// 4. Iterate through the array.
+		// For every pixel in the list, fill in a rectangle using the pixel's color.
+		// Then, use drawRect to add a grid pattern to your display.
+		for(int i = 0; i < array2d[rows-1].length; i++) {
+			for(int j = 0; j < array2d[cols-1].length; j++) {
+				g.setColor(array2d[i][j].color);
+				g.fillRect(array2d[i][j].x, array2d[i][j].y, pixelWidth, pixelHeight);
+				g.setColor(color.white);
+				g.drawRect(array2d[i][j].x, array2d[i][j].y, pixelWidth, pixelHeight);
+			}
+		}
 
-
-    }
-
-    public void setColor(Color c) {
-        color = c;
-    }
-
-    public void clickPixel(int mouseX, int mouseY) {
-        // 5. Use the mouseX and mouseY variables to change the color
-        //    of the pixel that was clicked. *HINT* Use the pixel's dimensions.
-    }
-
-    @Override
-    public void paintComponent(Graphics g) {
-        // 4. Iterate through the array.
-        //    For every pixel in the list, fill in a rectangle using the pixel's color.
-        //    Then, use drawRect to add a grid pattern to your display.
-
-    }
+	}
 }
